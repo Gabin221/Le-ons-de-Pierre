2886301278e36499be8f5b50fb36790bc1e15a78
Gabin221 <serrurot19@gmail.com>
2024-06-13 12:52:58 +0200

    Update

---
+++ Images/resolutionEquationSecondDegre.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def hello():
+	x = np.linspace(-4, 3, 1000)
+	y = 4*x**2 + 5*x - 4
+
+	plt.figure()
+	plt.axhline(color = 'k')
+	plt.axvline(color='k')
+	plt.plot(x, y, label=r"$ f(x) = 4x^{2} + 5x - 4 $")
+	plt.plot([(-5 - np.sqrt(89))/8], [0], marker="o", label=r"$ x_{1} = \frac{-5 - \sqrt{89}}{8} $")
+	plt.plot([(-5 + np.sqrt(89))/8], [0], marker="o", label=r"$ x_{2} = \frac{-5 + \sqrt{89}}{8} $")
+	plt.grid(True)
+	plt.legend()
+	plt.savefig("resolutionEquationSecondDegre.png")
+	plt.show()
+
+
+if __name__ == "__main__":
+	hello()
